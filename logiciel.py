1498a4f264b5929d40bc6e606ec133bd2b809b5c
Lilian <lil.bouhier@gmail.com>
2025-01-01 15:26:39 +0100

    ajout de la boucle du jeu et correction de la couleur du background

--- logiciel.py
+++ logiciel.py
@@ -6,5 +6,16 @@ largeur = 400
 hauteur = 400
 screen = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("logiciel de dessin")
-blanc = pygame.Color(255, 255, 255)
-screen.fill(blanc)
+noir = pygame.Color(0, 0, 0)
+screen.fill(noir)
+
+running = True
+while running :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_ESCAPE :
+                running = False
+
+pygame.quit()
