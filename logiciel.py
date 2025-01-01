5160141cadfe2d0f83a8bf5631e059eb71d0b381
Lilian <lil.bouhier@gmail.com>
2025-01-01 15:57:51 +0100

    ajout d'un dictionnaire pour les couleurs

--- logiciel.py
+++ logiciel.py
@@ -1,14 +1,20 @@
 import pygame
 pygame.init()
 
+couleur = {
+    "noir" : pygame.Color(0, 0, 0),
+    "blanc" : pygame.Color(255, 255, 255),
+    "rouge" : pygame.Color(255, 0, 0),
+    "vert" : pygame.Color(0, 255, 0),
+    "bleu" : pygame.Color(0, 0, 255)
+}
+
 # Fenêtre
 largeur = 400
 hauteur = 400
 screen = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("logiciel de dessin")
-noir = pygame.Color(0, 0, 0)
-blanc = pygame.Color(255, 255, 255)
-screen.fill(blanc)
+screen.fill(couleur["blanc"])
 
 running = True
 debut_ligne = (0, 0)
@@ -22,7 +28,7 @@ while running :
         elif event.type == pygame.MOUSEMOTION:
             fin_ligne = pygame.mouse.get_pos()
             if pygame.mouse.get_pressed() == (1, 0, 0):
-                pygame.draw.line(screen, noir, debut_ligne, fin_ligne, 1)
+                pygame.draw.line(screen, couleur["noir"], debut_ligne, fin_ligne, 1)
             debut_ligne = fin_ligne
         pygame.display.flip()
 
